b29cd12f3fd28dfe3292d3425518d8729b111f4f
Yeray <2023_yeray.reyes@iticbcn.cat>
2023-12-16 01:06:34 +0100

    añadidas funciones de personalización del usuario - en desarrollo

--- .extra_files/script.js
+++ .extra_files/script.js
@@ -13,6 +13,19 @@ fetch('../config/config.ini')
         document.getElementById('title1').textContent = `${datos.section1.title1}`;
         document.getElementById('title1_description').textContent = `${datos.section1.title1_description}`;
 
+        //Studies
+        document.getElementById('studies_title').textContent = `${datos.studies.studies_title}`;
+
+        document.getElementById('year_studies1').textContent = `${datos.studies.year_studies1}`;
+        document.getElementById('title_studies1').textContent = `${datos.studies.title_studies1}`;
+        document.getElementById('institution_studies1').textContent = `${datos.studies.institution_studies1}`;
+        document.getElementById('studies1_link').href = datos.studies.studies1_link
+
+        document.getElementById('year_studies2').textContent = `${datos.studies.year_studies2}`;
+        document.getElementById('title_studies2').textContent = `${datos.studies.title_studies2}`;
+        document.getElementById('institution_studies2').textContent = `${datos.studies.institution_studies2}`;
+        document.getElementById('studies2_link').href = datos.studies.studies2_link
+
         //Colors
         document.documentElement.style.setProperty('--color-separator', datos.colors.separator_color);
         document.documentElement.style.setProperty('--background_content', datos.colors.background_content_color)
